337ad69e578d065c01738a571dae86e4094c5c6b
Z4NO <danixt35@gmail.com>
2024-10-01 20:20:43 +0200

    Push de los archivos

---
+++ Daily TRAIN/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Daily_TRAIN
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Obtener el botón que fue clickeado
+            Button button = sender as Button;
+            string muscleGroup = button.Content.ToString();
+
+            // Simular el entrenamiento de esa semana basado en el grupo muscular seleccionado
+            switch (muscleGroup)
+            {
+                case "Pecho":
+                    Modelo_Ejercios modelo_Ejercios = new Modelo_Ejercios();
+                    modelo_Ejercios.Show();
+
+                    break;
+                case "Espalda":
+                    
+                    break;
+                case "Brazo":
+                    
+                    break;
+                case "Pierna":
+                    
+                    break;
+            }
+        }
+    }
+}
---
+++ Daily TRAIN/Modelo_Ejercios.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Daily_TRAIN
+{
+    /// <summary>
+    /// Lógica de interacción para Modelo_Ejercios.xaml
+    /// </summary>
+    public partial class Modelo_Ejercios : Window
+    {
+        public Modelo_Ejercios()
+        {
+            InitializeComponent();
+            CurrentDateLabel.Text = DateTime.Now.ToString("dddd, dd MMMM yyyy");
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
